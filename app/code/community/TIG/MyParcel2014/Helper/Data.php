061e5e85dae9cf98ecf6a027e5760d5a9295922a
reindertvetter <reindert@myparcel.nl>
2016-05-07 09:38:48 +0200

    bol_com

--- app/code/community/TIG/MyParcel2014/Helper/Data.php
+++ app/code/community/TIG/MyParcel2014/Helper/Data.php
@@ -216,7 +216,7 @@
         {
             $myParcelShippingMethods = $this->getMyParcelShippingMethods();
 
-            $method = 'bolcom_bolcom' !== $method ? 'bolcom_flatrate' : $method;
+            $method = 'bolcom_bolcom' === $method ? 'bolcom_flatrate' : $method;
             $method = strpos($method, 'matrixrate_matrixrate') !== false ? 'matrixrate_matrixrate' : $method;
             if (in_array($method, $myParcelShippingMethods)) {
                 return true;
