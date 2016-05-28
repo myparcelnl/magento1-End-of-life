9a96cf767830762186f83235ad46982c0540bc35
reindertvetter <reindert@myparcel.nl>
2016-05-28 13:30:10 +0200

    new MyParcel options in checkout

---
+++ skin/frontend/base/default/js/TIG/MyParcel2014/checkout.js
@@ -0,0 +1,56 @@
+/**
+ * LICENSE: This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ *
+ * If you want to add improvements, please create a fork in our GitHub:
+ * https://github.com/myparcelnl
+ *
+ * @author      Reindert Vetter <reindert@myparcel.nl>
+ * @copyright   2010-2016 MyParcel
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US  CC BY-NC-ND 3.0 NL
+ * @link        https://github.com/myparcelnl/magento1
+ * @since       File available since Release 1.6.0
+ */
+(function() {
+
+    /* Set up the mutation observer */
+    var myParcelObserver = new MutationObserver(function (mutations, me) {
+
+        var canvas = document.getElementById('s_method_myparcel_flatrate');
+        if (canvas) {
+            jQuery(document).ready(
+                myParcelOptions.initialize(),
+                me.disconnect() /* stop observing */
+            )
+        }
+
+    });
+
+    /* start observing */
+    myParcelObserver.observe(document, {
+        childList: true,
+        subtree: true
+    });
+
+    /* Load MyParcel options */
+    var myParcelOptions = {
+
+        initialize: function () {
+
+            jQuery('#s_method_myparcel_flatrate').parents(':eq(2)').hide();
+            jQuery('#checkout-shipping-method-load').append(jQuery('#mypa-delivery-options-container'));
+
+            jQuery("input:radio[name='mypa-delivery-time']").on('change', function () {
+                console.log($(this).id)
+            });
+
+            jQuery("input:checkbox[name='mypa-onoffswitch']").on('change', function () {
+                console.log($(this).checked)
+            });
+
+        }
+
+    };
+
+}).call(this);
