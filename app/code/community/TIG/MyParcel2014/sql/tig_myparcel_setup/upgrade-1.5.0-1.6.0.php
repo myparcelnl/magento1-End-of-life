f655c942bc88d99ba858388a4941fbbb3745741a
reindertvetter <reindert@myparcel.nl>
2016-06-10 12:03:18 +0200

    myparcel_date sql

--- app/code/community/TIG/MyParcel2014/sql/tig_myparcel_setup/upgrade-1.5.0-1.6.0.php
+++ app/code/community/TIG/MyParcel2014/sql/tig_myparcel_setup/upgrade-1.5.0-1.6.0.php
@@ -17,4 +17,30 @@
             )
         );
     }
+    $tableName = $installer->getTable('sales/quote');
+    if (!$conn->tableColumnExists($tableName, 'myparcel_data')) {
+        $conn->addColumn(
+            $tableName,
+            'myparcel_data',
+            array(
+                'type'     => Varien_Db_Ddl_Table::TYPE_VARCHAR,
+                'nullable' => true,
+                'default'  => null,
+                'comment'  => 'Checkout MyParcel data',
+            )
+        );
+    }
+    $tableName = $installer->getTable('sales/order');
+    if (!$conn->tableColumnExists($tableName, 'myparcel_data')) {
+        $conn->addColumn(
+            $tableName,
+            'myparcel_data',
+            array(
+                'type'     => Varien_Db_Ddl_Table::TYPE_VARCHAR,
+                'nullable' => true,
+                'default'  => null,
+                'comment'  => 'Checkout MyParcel data',
+            )
+        );
+    }
     $installer->endSetup();
