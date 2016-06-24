0645fc318ed156b88ab17071166e6b45f451429c
reindertvetter <reindert@myparcel.nl>
2016-06-24 11:46:21 +0200

    revert for uppercase

---
+++ app/code/community/TIG/MyParcel2014/Model/System/Config/Source/DelayDays.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ *                  ___________       __            __
+ *                  \__    ___/____ _/  |_ _____   |  |
+ *                    |    |  /  _ \\   __\\__  \  |  |
+ *                    |    | |  |_| ||  |   / __ \_|  |__
+ *                    |____|  \____/ |__|  (____  /|____/
+ *                                              \/
+ *          ___          __                                   __
+ *         |   |  ____ _/  |_   ____ _______   ____    ____ _/  |_
+ *         |   | /    \\   __\_/ __ \\_  __ \ /    \ _/ __ \\   __\
+ *         |   ||   |  \|  |  \  ___/ |  | \/|   |  \\  ___/ |  |
+ *         |___||___|  /|__|   \_____>|__|   |___|  / \_____>|__|
+ *                  \/                           \/
+ *                  ________
+ *                 /  _____/_______   ____   __ __ ______
+ *                /   \  ___\_  __ \ /  _ \ |  |  \\____ \
+ *                \    \_\  \|  | \/|  |_| ||  |  /|  |_| |
+ *                 \______  /|__|    \____/ |____/ |   __/
+ *                        \/                       |__|
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@tig.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@tig.nl for more information.
+ *
+ * @copyright   Copyright (c) 2014 Total Internet Group B.V. (http://www.totalinternetgroup.nl)
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+
+class TIG_MyParcel2014_Model_System_Config_Source_DelayDays
+{
+    /**
+     * Source model for yes / no setting.
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        $helper = Mage::helper('tig_myparcel');
+
+        $array = array(
+            array(
+                'value' => 0,
+                'label' => $helper->__('No delay'),
+            ),
+            array(
+                'value' => 1,
+                'label' => 1 . ' ' . $helper->__('day'),
+            ),
+        );
+
+
+        $x = 2;
+        while($x <= 14) {
+            $array[] = array(
+                'value' => $x,
+                'label' => $x . ' ' . $helper->__('days')
+            );
+            $x++;
+        }
+
+        return $array;
+    }
+}
---
+++ app/code/community/TIG/MyParcel2014/Model/System/Config/Source/DropOffDays.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ *                  ___________       __            __
+ *                  \__    ___/____ _/  |_ _____   |  |
+ *                    |    |  /  _ \\   __\\__  \  |  |
+ *                    |    | |  |_| ||  |   / __ \_|  |__
+ *                    |____|  \____/ |__|  (____  /|____/
+ *                                              \/
+ *          ___          __                                   __
+ *         |   |  ____ _/  |_   ____ _______   ____    ____ _/  |_
+ *         |   | /    \\   __\_/ __ \\_  __ \ /    \ _/ __ \\   __\
+ *         |   ||   |  \|  |  \  ___/ |  | \/|   |  \\  ___/ |  |
+ *         |___||___|  /|__|   \_____>|__|   |___|  / \_____>|__|
+ *                  \/                           \/
+ *                  ________
+ *                 /  _____/_______   ____   __ __ ______
+ *                /   \  ___\_  __ \ /  _ \ |  |  \\____ \
+ *                \    \_\  \|  | \/|  |_| ||  |  /|  |_| |
+ *                 \______  /|__|    \____/ |____/ |   __/
+ *                        \/                       |__|
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@tig.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@tig.nl for more information.
+ *
+ * @copyright   Copyright (c) 2014 Total Internet Group B.V. (http://www.totalinternetgroup.nl)
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+
+class TIG_MyParcel2014_Model_System_Config_Source_DropOffDays
+{
+    /**
+     * Source model for customs setting.
+     *
+     * @return array
+     */
+    public function toOptionArray($isMultiSelect = false, $isActiveOnlyFlag = false)
+    {
+        $helper = Mage::helper('tig_myparcel');
+
+        $array = array(
+            array(
+                'value' => 1,
+                'label' => $helper->__('Saturday, Sunday, Monday'),
+            ),
+            array(
+                'value' => 2,
+                'label' => $helper->__('Tuesday'),
+            ),
+            array(
+                'value' => 3,
+                'label' => $helper->__('Wednesday'),
+            ),
+            array(
+                'value' => 4,
+                'label' => $helper->__('Thursday'),
+            ),
+            array(
+                'value' => 5,
+                'label' => $helper->__('Friday'),
+            ),
+        );
+        return $array;
+    }
+}
