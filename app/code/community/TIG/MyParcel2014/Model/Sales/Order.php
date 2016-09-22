3ad700458a4ec50b29ff2e29a2d54f4ec714d198
reindertvetter <reindert@myparcel.nl>
2016-09-22 16:42:32 +0200

    Remove option for Afterpay

--- app/code/community/TIG/MyParcel2014/Model/Sales/Order.php
+++ app/code/community/TIG/MyParcel2014/Model/Sales/Order.php
@@ -27,8 +27,7 @@
          */
         public function getShippingAddress() {
 
-            $helper = Mage::helper('tig_myparcel');
-            $usePgAddress = $helper->getConfig('pakjegemak_use_shipment_address') === '1';
+            $usePgAddress = true;
 
             $parentFunctions = debug_backtrace();
             if($parentFunctions[3]['class'] != 'TIG_Afterpay_Model_PaymentFee_Observer'){
