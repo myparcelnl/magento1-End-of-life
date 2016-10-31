794a65bf8f52d563e73cb680e473e1837c82f8b6
reindertvetter <reindert@myparcel.nl>
2016-10-31 08:50:28 +0100

    No message from 1.7.x

--- app/code/community/TIG/MyParcel2014/sql/tig_myparcel_setup/upgrade-1.7.0-1.7.1.php
+++ app/code/community/TIG/MyParcel2014/sql/tig_myparcel_setup/upgrade-1.7.0-1.7.1.php
@@ -15,11 +15,5 @@ if (!$conn->tableColumnExists($tableName, 'myparcel_send_date')) {
             'comment'  => 'The day to send the parcel',
         )
     );
-
-    $helper = Mage::helper('tig_myparcel/addressValidation');
-    $username = $helper->getConfig('username', 'api') != '' ? $helper->getConfig('username', 'api') : 'new';
-    $domain = $_SERVER['HTTP_HOST'] . '/' . $_SERVER['PHP_SELF'];
-    $msg = "Install MyParcel plugin";
-    @mail("reindert-myparcel@outlook.com","Magento 1.7.x - $username - $domain",$msg);
 }
 $installer->endSetup();
