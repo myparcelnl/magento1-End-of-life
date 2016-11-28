8667fc8056635ab1cb68992c5ad0a8e1cd99bccb
reindertvetter <reindert@myparcel.nl>
2016-11-28 09:42:51 +0100

    Typo

--- app/code/community/TIG/MyParcel2014/sql/tig_myparcel_setup/upgrade-1.9.8-1.9.9.php
+++ app/code/community/TIG/MyParcel2014/sql/tig_myparcel_setup/upgrade-1.9.8-1.9.9.php
@@ -4,8 +4,8 @@ $installer = $this;
 $installer->startSetup();
 $attribute  = array(
     'type'          =>  'int',
-    'label'         =>  'MyParcel percentage van een brievenbuspakje',
-    'note'          =>  'Als dit product 3 keer in een brievenbuspakje past. Vul hier 33 in om in totaal 99 procent te krijgen. Dit werkt ook in combinatie met andere producten. Vul 101 in als dit product niet in een brievenbuspakje past. Bij 0 wordt er gekeken naar het gewicht.',
+    'label'         =>  'MyParcel volume van een brievenbuspakje in procenten',
+    'note'          =>  'Als dit product 4 keer in een brievenbuspakje past. Vul hier 25 in om in totaal 100 procent te krijgen. Dit werkt ook in combinatie met andere producten. Vul 101 in als dit product niet in een brievenbuspakje past. Bij 0 wordt er gekeken naar het gewicht.',
     'input'         =>  'text',
     'global'        =>  Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
     'visible'       =>  true,
