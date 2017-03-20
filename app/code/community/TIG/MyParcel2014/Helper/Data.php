9a1c53d6868baa0a3e4dd861c1e59485bef3d769
reindertvetter <reindert@myparcel.nl>
2017-03-20 15:27:57 +0100

    Fix order grid always has mailbox

--- app/code/community/TIG/MyParcel2014/Helper/Data.php
+++ app/code/community/TIG/MyParcel2014/Helper/Data.php
@@ -550,7 +550,6 @@ class TIG_MyParcel2014_Helper_Data extends Mage_Core_Helper_Abstract
             if ($hasExtraOptions || $hideMailboxInFrontend == true) {
                 $type = 1;
             } else {
-
                 $fitInLetterbox = $this->fitInLetterbox($items);
                 $type = $fitInLetterbox && $country == 'NL' ? 2 : 1;
             }
@@ -577,6 +576,7 @@ class TIG_MyParcel2014_Helper_Data extends Mage_Core_Helper_Abstract
 
         foreach ($items as $item) {
             $qty = $item->getQty();
+            $qty = $qty == null ? $item->getData('qty_ordered') : $qty;
             if ($item instanceof Mage_Sales_Model_Order_Shipment_Item) {
                 /** @var Mage_Sales_Model_Order_Item $item */
                 $id = $item->getProductId();
