1eb6632808aad2e797938de0c64d8cd718270dc0
Reindert <reindert@myparcel.nl>
2017-08-04 09:20:27 +0200

    Revert "Get checkout lib from myparcenl/checkout"

--- app/design/frontend/base/default/template/TIG/MyParcel2014/checkout/mypa_custom_css.php
+++ app/design/frontend/base/default/template/TIG/MyParcel2014/checkout/mypa_custom_css.php
@@ -3,26 +3,63 @@
     /* Set base color */
     <?php $baseColor = $helper->getConfig('base_color', 'checkout');?>
     <?php if($baseColor != ''):?>
-        .mypa-tab, .mypa-date:checked + label, .mypa-tab:hover, .mypa-delivery-header, .mypa-delivery-header, .mypa-date:checked+label, .mypa-tab:hover, #mypa-no-options {
-            background-color: <?php echo $baseColor;?>
+
+        .mypa-tab{
+            background-color: #<?php echo $baseColor;?>;
+            opacity: .5;
         }
+
+        .mypa-delivery-header,
+        .mypa-date:checked+label, .mypa-tab:hover
+        {
+            background: #<?php echo $baseColor;?>;
+            opacity: 1;
+        }
+
         .mypa-address {
-            color: <?php echo $baseColor;?>
+            color: #<?php echo $baseColor;?>;
+        }
+
+        .edit-tip > div {
+            border-top-color: #<?php echo $baseColor;?>;;
+        }
+
+        .edit-stem {
+            background-color: #<?php echo $baseColor;?>;
+        }
+
+        #mypa-no-options {
+            background: #<?php echo $baseColor;?>;
         }
     <?php endif;?>
 
     /* Set select color */
     <?php $selectColor = $helper->getConfig('select_color', 'checkout');?>
     <?php if($selectColor != ''):?>
-        .mypa-circle:hover, label.mypa-row-subitem:hover .mypa-circle, input:checked + label.mypa-checkmark div.mypa-circle, input[name=mypa-delivery-type]:checked + label div.mypa-main div.mypa-circle, input[name=mypa-pickup-option]:checked + label div.mypa-main div.mypa-circle, input:checked ~ .mypa-price, input:checked ~ label span.mypa-price, input:checked ~ span span.mypa-price, .mypa-price-active {
-            background-color: <?php echo $selectColor;?>;
+
+        input:checked ~ .mypa-highlight, input:checked ~ label.mypa-row-title span.mypa-highlight,
+        .mypa-arrow-clickable:hover
+        {
+            color: #<?php echo $selectColor; ?>;
         }
-        input:checked ~ .mypa-highlight, input:checked ~ label.mypa-row-title span.mypa-highlight, .mypa-arrow-clickable:hover, .mypa-pickup-selector input:checked ~ .mypa-highlight, .mypa-edit-location {
-            color: <?php echo $selectColor;?>;
+
+        input:checked + label.mypa-checkmark div.mypa-circle, input[name=mypa-delivery-type]:checked + label div.mypa-main div.mypa-circle, input[name=mypa-pickup-option]:checked + label div.mypa-main div.mypa-circle,
+        .mypa-circle:hover, label.mypa-row-subitem:hover .mypa-circle,
+        input:checked ~ .mypa-price, input:checked ~ span span.mypa-price
+        {
+            background-color: #<?php echo $selectColor; ?>;
         }
-        .mypa-arrow-clickable:hover::before {
-            border-left: 0.2em solid <?php echo $selectColor;?>;
-            border-bottom: 0.2em solid <?php echo $selectColor;?>;
+
+        .mypa-arrow-clickable:hover::before{
+            border-left: 0.2em solid #<?php echo $selectColor;?>;
+            border-bottom: 0.2em solid #<?php echo $selectColor;?>;
+        }
+
+        input:checked ~ .mypa-price, input:checked ~ span span.mypa-price, input:checked ~ label span.mypa-price, .mypa-price-active {
+            background: #<?php echo $selectColor;?>;
+        }
+        .edit-location {
+            color: #<?php echo $selectColor;?>;
         }
     <?php endif;?>
     <?php $numberOfDays = $helper->getConfig('deliverydays_window', 'checkout');?>
