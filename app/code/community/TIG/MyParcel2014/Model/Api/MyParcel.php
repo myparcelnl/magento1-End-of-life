854f370859d4b661756f9b2c13133a77bb265798
Reindert <reindert@myparcel.nl>
2017-08-21 13:20:50 +0200

    remove exit

--- app/code/community/TIG/MyParcel2014/Model/Api/MyParcel.php
+++ app/code/community/TIG/MyParcel2014/Model/Api/MyParcel.php
@@ -349,8 +349,6 @@ class TIG_MyParcel2014_Model_Api_MyParcel extends Varien_Object
             // log the request url
             $helper->log($url);
             $helper->log(json_decode($body));
-            echo($body);
-            exit;
             $request->setConfig($config)
                 ->write(Zend_Http_Client::POST, $url, '1.1', $header, $body);
         } else {
