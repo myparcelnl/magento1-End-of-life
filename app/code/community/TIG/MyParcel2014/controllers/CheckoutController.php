96e7afdbd7631a4bd7da55c7e4b3696f1cf2483d
Richard Perdaan <richard@myparcel.nl>
2018-10-26 11:39:49 +0200

    remove whitespace

--- app/code/community/TIG/MyParcel2014/controllers/CheckoutController.php
+++ app/code/community/TIG/MyParcel2014/controllers/CheckoutController.php
@@ -123,7 +123,6 @@ class TIG_MyParcel2014_CheckoutController extends Mage_Core_Controller_Front_Act
         $delivery['signature_and_only_recipient_fee'] =                $this->getShippingPrice($helper->getConfig('signature_and_only_recipient_fee', 'delivery'), $quote);
         $data['delivery'] = (object)$delivery;
 
-
         $morningDelivery['active'] =                $helper->getConfig('morning_delivery_active', 'morning_delivery') == "1" && $helper->getConfig('age_check', 'delivery') != "1" && $data['address']['country'] == 'NL' ? true : false;
         $morningDelivery['morning_delivery_titel'] = $helper->getConfig('morning_delivery_titel', 'morning_delivery');
         $morningDelivery['fee'] =                   $this->getExtraPrice($basePrice, $this->getShippingPrice($helper->getConfig('morning_delivery_fee', 'morning_delivery'), $quote));
